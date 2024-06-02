eddffb89e9a1f67b19aa0071d6d55095c8769e8d
0x2170 <151378415+DLLenjoyer@users.noreply.github.com>
2024-06-02 22:53:33 +0200

    Update models.go

--- server/models/models.go
+++ server/models/models.go
@@ -1,1 +1,15 @@
+package models
 
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name  string `gorm:"not null"`
+	Email string `gorm:"unique;not null"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
